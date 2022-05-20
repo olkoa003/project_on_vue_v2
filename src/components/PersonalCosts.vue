dc072a7e309fdb29a63f20dca2f806bbeb7d2ff7
olkoa003 <43721193+olkoa003@users.noreply.github.com>
2022-05-20 14:13:33 +0100

    my calc is ready

---
+++ src/components/PersonalCosts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="costs">
+    <h1>My Personal Costs Page</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "PersonalCosts"
+};
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>My Personal Costs Page</h1>
+      <PersonalCosts/>
   </div>
 </template>
+
+<script>
+import PersonalCosts from "@/components/PersonalCosts.vue";
+
+export default {
+  name: "AboutView",
+  components: {
+    PersonalCosts
+  },
+}
+</script>
